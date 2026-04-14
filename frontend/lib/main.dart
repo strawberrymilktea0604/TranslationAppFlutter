9f56724f1e57ec34055badea3d61744d592edfdc
TrinhQuynhAnh <anh0279367@huce.edu.vn>
2026-04-14 07:59:25 +0700

    modified:   frontend/lib/main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart' hide State;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:frontend/l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
 import 'package:frontend/core/error/failures.dart';
 import 'package:frontend/core/router/app_router.dart';
