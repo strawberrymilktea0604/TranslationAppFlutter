485b4ef16cbba8caae16898220ba39bd52422898
strawberrymilktea0604 <4004267@st.huce.edu.vn>
2026-04-14 09:54:34 +0700

    sửa lỗi namespace fe

--- frontend/android/build.gradle.kts
+++ frontend/android/build.gradle.kts
@@ -15,10 +15,28 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+subprojects {
+    val configureNamespace = {
+        if (extensions.findByName("android") != null) {
+            val android = extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            if (android.namespace.isNullOrEmpty()) {
+                android.namespace = project.group.toString()
+            }
+        }
+    }
+
+    // Kiểm tra xem project đã evaluate xong chưa
+    if (state.executed) {
+        configureNamespace() // Nếu xong rồi thì chạy luôn
+    } else {
+        afterEvaluate { configureNamespace() } // Nếu chưa thì đặt lịch chờ
+    }
 }
